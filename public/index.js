0fc12986377a12bf3bb018dc46e469fe2ec3564a
Nimish Santosh <nimishsantosh107@icloud.com>
2019-08-03 18:23:18 +0530

    screen record works

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
-mediaConfig = {audio: true, video: true};
-navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream){
+const vbideo = document.querySelector("#video");
+
+mediaConfig = {};
+navigator.mediaDevices.getDisplayMedia(mediaConfig).then(function(stream){
 	window.stream = stream;
+	video.srcObject = stream;
 },function(err){console.log("GET_USR_MEDIA_ERR: ",err);});
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const http = require('http');
 const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser')
 const path = require('path');
 
-const PORT = 80;
+const PORT = 2000;
 const IP = "192.168.1.4";
-const privateKey  = fs.readFileSync('/rtc.key', 'utf8');
-const certificate = fs.readFileSync('/rtc.csr', 'utf8');
+const privateKey  = fs.readFileSync('./SSL_KEY/rtc.key', 'utf8');
+const certificate = fs.readFileSync('./SSL_KEY/rtc.crt', 'utf8');
 const credentials = {key: privateKey, cert: certificate};
 
 var app = express();
 app.use(bodyParser.json());
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
 
 app.use('/',express.static(path.join(__dirname,"public")));
 
-app.listen(PORT, IP, ()=>{console.log(`SERVER UP ON PORT: ${PORT}`);});
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(PORT, IP, ()=>{console.log(`HTTPS SERVER UP ON PORT: ${PORT}`);});
