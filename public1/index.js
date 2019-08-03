ce86923427cb82f33e06984ec15a75415386c446
Nimish Santosh <nimishsantosh107@icloud.com>
2019-08-03 22:55:07 +0530

    peerjs

--- public1/index.js
+++ public1/index.js
@@ -2,21 +2,84 @@ const socket = io();
 const shareBtn = document.querySelector("#shareBtn");
 const signalBtn = document.querySelector("#signalBtn");
 
+shareBtn.addEventListener('click', function(){
+	mediaConfig = {};
+	navigator.mediaDevices.getDisplayMedia(mediaConfig).then(function(stream){
+		window.stream = stream;
+		// video.srcObject = stream;
+	},function(err){console.log("GET_USR_MEDIA_ERR: ",err);});
+});
+
+signalBtn.addEventListener('click', function () {
+	socket.emit("sendSignal",peerid);
+});
+
+socket.on("connect", function () {console.log('CONNECTED TO SERVER');});
+
+socket.on("receivedSignal", function (data) {
+	console.log('RECEIVED SIGNAL: ',data);
+
+	conn = peer.connect(data);
+
+	conn.on('open', function() {
+		// Receive messages
+		conn.on('data', function(data) {
+			console.log('RECEIVED:', data);
+		});
+
+		//SEND MSG
+		conn.send("HIII");
+	});
+});
+
+var peer = new Peer();
+var peerid = null;
+
+peer.on('open', function(id) {
+  console.log('PEER ID: ' + id);
+  peerid = id;
+});
+
+peer.on('connection', function(conn) {
+	console.log(conn);
+
+	conn.on('open', function() {
+	  	// Receive messages
+		conn.on('data', function(data) {
+		    console.log('RECEIVED:', data);
+		});
+
+		//SEND MSG
+	});
+});
+
+//SIMPLE_PEER
+/*
 var simplePeerConfig = {
   initiator: true,
   channelConfig: {},
   channelName: '<random string>',
   config: { iceServers: [
-	  	{
-            urls: "stun:numb.viagenie.ca",
-            username: "nimishsantosh107@gmail.com",
-            credential: "messi107"
-        },
-        {
-            urls: "turn:numb.viagenie.ca",
-            username: "nimishsantosh107@gmail.com",
-            credential: "messi107"
-        }
+  	{
+	    urls: 'turn:192.158.29.39:3478?transport=udp',
+	    username: '28224511:1379330808',
+	    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
+	},
+	{
+	    urls: 'turn:192.158.29.39:3478?transport=tcp',
+	    username: '28224511:1379330808',
+	    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
+	},
+  	{
+        urls: "stun:numb.viagenie.ca",
+        username: "nimishsantosh107@gmail.com",
+        credential: "messi107"
+    },
+    {
+        urls: "turn:numb.viagenie.ca",
+        username: "nimishsantosh107@gmail.com",
+        credential: "messi107"
+    }
   	] },
   offerOptions: {},
   answerOptions: {},
@@ -31,13 +94,6 @@ var simplePeerConfig = {
 
 var p = new SimplePeer(simplePeerConfig); 
 var signalData = null;
-
-socket.on("connect", function () {console.log('CONNECTED TO SERVER');});
-
-socket.on("receivedSignal", function (data) {
-	console.log('RECEIVED SIGNAL: ',data);
-	//NOT SETING P.SIGNAL
-});
 
 p.on("error", function (err){console.log('P_ON_ERROR: ',err);});
 
@@ -56,15 +112,4 @@ p.on("connect", function(){
 p.on("data", function(data){
   	console.log('DATA: ',data);
 });
-
-signalBtn.addEventListener('click', function () {
-	socket.emit("sendSignal",JSON.stringify(signalData));
-});
-
-shareBtn.addEventListener('click', function(){
-	mediaConfig = {};
-	navigator.mediaDevices.getDisplayMedia(mediaConfig).then(function(stream){
-		window.stream = stream;
-		// video.srcObject = stream;
-	},function(err){console.log("GET_USR_MEDIA_ERR: ",err);});
-});
+*/
--- public2/index.js
+++ public2/index.js
@@ -1,21 +1,76 @@
 const socket = io();
 const signalBtn = document.querySelector("#signalBtn");
 
+signalBtn.addEventListener('click', function () {
+	socket.emit("sendSignal",peerid);
+});
+
+socket.on("connect", function () {console.log('CONNECTED TO SERVER');});
+
+socket.on("receivedSignal", function (data) {
+	console.log('RECEIVED SIGNAL: ',data);
+
+	conn = peer.connect(data);
+
+	conn.on('open', function() {
+		// Receive messages
+		conn.on('data', function(data) {
+			console.log('RECEIVED:', data);
+		});
+
+		//SEND MSG
+	});
+});
+
+var peer = new Peer();
+var peerid = null;
+
+peer.on('open', function(id) {
+  	console.log('PEER ID: ' + id);
+  	peerid = id;
+});
+
+peer.on('connection', function(conn) {
+	console.log(conn);
+
+	conn.on('open', function() {
+	  	// Receive messages
+		conn.on('data', function(data) {
+		    console.log('RECEIVED:', data);
+		});
+
+		//SEND MSG
+	});
+});
+
+
+//SIMPLE_PEER
+/*
 var simplePeerConfig = {
   initiator: false,
   channelConfig: {},
   channelName: '<random string>',
   config: { iceServers: [
-	  	{
-            urls: "stun:numb.viagenie.ca",
-            username: "nimishsantosh107@gmail.com",
-            credential: "messi107"
-        },
-        {
-            urls: "turn:numb.viagenie.ca",
-            username: "nimishsantosh107@gmail.com",
-            credential: "messi107"
-        }
+  	{
+	    urls: 'turn:192.158.29.39:3478?transport=udp',
+	    username: '28224511:1379330808',
+	    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
+	},
+	{
+	    urls: 'turn:192.158.29.39:3478?transport=tcp',
+	    username: '28224511:1379330808',
+	    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
+	},
+  	{
+        urls: "stun:numb.viagenie.ca",
+        username: "nimishsantosh107@gmail.com",
+        credential: "messi107"
+    },
+    {
+        urls: "turn:numb.viagenie.ca",
+        username: "nimishsantosh107@gmail.com",
+        credential: "messi107"
+    }
   	] },
   offerOptions: {},
   answerOptions: {},
@@ -30,13 +85,6 @@ var simplePeerConfig = {
 var p = new SimplePeer(simplePeerConfig);
 var signalData = null; 
 
-socket.on("connect", function () {console.log('CONNECTED TO SERVER');});
-
-socket.on("receivedSignal", function (data) {
-	console.log('RECEIVED SIGNAL: ',data);
-	p.signal(data);
-});
-
 p.on("error", function (err){console.log('P_ON_ERROR: ',err);});
 
 p.on("signal", function (data){
@@ -50,7 +98,4 @@ p.on("data", function(data){
   	console.log('DATA: ',data);
   	p.send("RECEIVED MESSAGE");
 });
-
-signalBtn.addEventListener('click', function () {
-	socket.emit("sendSignal",JSON.stringify(signalData));
-});
+*/
